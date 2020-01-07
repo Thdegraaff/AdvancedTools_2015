de0c7734d43851f5a6a8d6db7b073716bf282006
Thomas de Graaff <t.de.graaff@vu.nl>
2020-01-07 20:01:52 +0100

    do this to other set slides as well

---
+++ slides/.auctex-auto/02-AdvancedTopics.el
@@ -0,0 +1,60 @@
+(TeX-add-style-hook
+ "02-AdvancedTopics"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "ignorenonframetext") ("article" "12pt" "a4paper" "twocolumn")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("fontenc" "T1") ("inputenc" "utf8") ("hyperref" "colorlinks=true" "citecolor=magenta" "urlcolor=magenta") ("babel" "english" "german")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "minted")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "Table"
+    "beamer"
+    "beamer10"
+    "amssymb"
+    "amsmath"
+    "fontenc"
+    "inputenc"
+    "microtype"
+    "longtable"
+    "booktabs"
+    "babel"
+    "pgf"
+    "times"
+    "etex"
+    "dcolumn"
+    "minted"
+    "tcolorbox"
+    "hyperref"
+    "article"
+    "art12"
+    "art10"
+    "multicol"
+    "lipsum")
+   (LaTeX-add-labels
+    "introduction"
+    "introduction-1"
+    "fig:ligatures"
+    "tab:example"
+    "tab:example2"
+    "tab:grades")
+   (LaTeX-add-bibliographies
+    "mybib.bib")
+   (LaTeX-add-xcolor-definecolors
+    "links"
+    "bashcodebg")
+   (LaTeX-add-tcbuselibraries
+    "minted,skins")
+   (LaTeX-add-array-newcolumntypes
+    "d"))
+ :latex)
+
