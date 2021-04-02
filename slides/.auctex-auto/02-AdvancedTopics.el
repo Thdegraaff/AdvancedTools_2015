f4d6875f95de84267802faa585691974dbd3d994
Thomas de Graaff <t.de.graaff@vu.nl>
2021-04-02 11:23:58 +0200

    solve error

--- slides/.auctex-auto/02-AdvancedTopics.el
+++ slides/.auctex-auto/02-AdvancedTopics.el
@@ -5,18 +5,19 @@
                      '(("beamer" "ignorenonframetext") ("article" "12pt" "a4paper" "twocolumn")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("fontenc" "T1") ("inputenc" "utf8") ("hyperref" "colorlinks=true" "citecolor=magenta" "urlcolor=magenta") ("babel" "english" "german")))
-   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
    (add-to-list 'LaTeX-verbatim-environments-local "minted")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
+    "table"
     "Table"
     "beamer"
     "beamer10"
@@ -52,9 +53,9 @@
    (LaTeX-add-xcolor-definecolors
     "links"
     "bashcodebg")
-   (LaTeX-add-tcbuselibraries
-    "minted,skins")
    (LaTeX-add-array-newcolumntypes
-    "d"))
+    "d")
+   (LaTeX-add-tcbuselibraries
+    "minted,skins"))
  :latex)
 
